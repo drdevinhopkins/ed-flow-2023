edb1272ad712885a471c943f701d95b8763ed9f7
drdevinhopkins <41838748+drdevinhopkins@users.noreply.github.com>
2023-11-17 18:41:35 +0000

    .

--- scripts/get_current.py
+++ scripts/get_current.py
@@ -5,7 +5,6 @@ import os
 import pandas as pd
 from deta import Deta
 from dotenv import load_dotenv
-
 import constants
 
 load_dotenv()
@@ -18,10 +17,10 @@ url = 'https://www.dropbox.com/s/ckijmipu33z3feg/HourlyReport.pdf?dl=1'
 r = requests.get(url, allow_redirects=True)
 open('hourlyreport.pdf', 'wb').write(r.content)
 
-tables = camelot.read_pdf('hourlyreport.pdf', flavor='stream', pages='1', columns=['41, 57, 75, 97.5, 115.5, 138, 160, 187, 202, 220, 238, 256, 279, 301, 323, 342, 360, 381, 400, 425, 443, 465, 483, 501, 519, 537, 559.5, 577.5, 600, 627.5, 652, 667, 690, 706.5, 730, 753, 778, 798, 822, 839.5, 855.5, 881, 906, 930, 956, 978'])
+tables = camelot.read_pdf('hourlyreport.pdf', pages= '1', flavor='stream', table_areas=['8,410,1000,50'])
 
-df = tables[0].df.loc[2:].reset_index(drop=True)
-df.columns = column_names
+df = tables[0].df.reset_index(drop=True)
+df.columns = constants.column_names
 df.dateflg = df.dateflg[df.dateflg.str.strip() != '']
 df.dateflg = df.dateflg.ffill()
 for column in df.columns.tolist():
@@ -36,3 +35,4 @@ df.to_csv('current.csv', index=False)
 
 data.put(name='current.csv', path='current.csv')
 
+print(df.head())
