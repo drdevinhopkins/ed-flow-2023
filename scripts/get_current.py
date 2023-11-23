4cf6ec1c3e27811101fd1908a53c4572088ea440
drdevinhopkins <41838748+drdevinhopkins@users.noreply.github.com>
2023-11-22 20:44:20 -0500

    allData

--- scripts/get_current.py
+++ scripts/get_current.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 deta = Deta(os.environ.get("DETA_PROJECT_KEY"))
 
-data = deta.Drive("data")
+drive = deta.Drive("data")
 
 URL = 'https://www.dropbox.com/s/ckijmipu33z3feg/HourlyReport.pdf?dl=1'
 r = requests.get(URL, allow_redirects=True)
@@ -33,11 +33,22 @@ for column in df.columns.tolist():
         continue
     df[column] = df[column].astype('float').astype('int')
 df["ds"] = pd.to_datetime(
-    df["dateflg"] + " " + (df["timeflg"] - 1).astype(str) + ":00", format='mixed') + datetime.timedelta(hours=1)
+    df["dateflg"] + " " + (df["timeflg"] - 1).astype(str) + ":00") + datetime.timedelta(hours=1)
 df = df.set_index('ds').reset_index().drop(['dateflg', 'timeflg'], axis=1)
 
 df.to_csv('current.csv', index=False)
 
-data.put(name='current.csv', path='current.csv')
+drive.put(name='current.csv', path='current.csv')
 
-print(df.head())
+df = df.sort_values(by='ds', ascending=True)
+
+allData = pd.read_csv('https://drive.deta.sh/v1/b0x22rtxtdf/data/files/download?name=allData.csv',
+                      storage_options={'X-API-Key': os.environ.get("DETA_PROJECT_KEY")})
+allData.ds = pd.to_datetime(allData.ds)
+
+allData = pd.concat([allData, df], ignore_index=True).drop_duplicates(
+    keep='last').sort_values(by='ds', ascending=True)
+
+drive.put(name='allData.csv', path='allData.csv')
+
+allData.tail(5)
